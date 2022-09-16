79a6c7b0dd06dde10987a0bc203202b2d00395ad
Ginett Rosales <g.rosale@uniandes.edu.co>
2022-09-15 20:03:35 -0500

    Agrega configuracion de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AerolineaModule } from './aerolinea/aerolinea.module';
 import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AerolineaModule, AeropuertoModule],
+  imports: [AerolineaModule, AeropuertoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'aerolineas',
+      entities: [AerolineaModule, AeropuertoModule],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
